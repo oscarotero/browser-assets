3877fc05d0754dbcbfb216668de807fd4edd6022
oscarotero <oom@oscarotero.com>
2018-06-16 12:31:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Browser assets
+
+Simple utility to copy files from `node_modules` to a public directory in order to be loaded by the browser.
+
+## Why?
+
+Because sometimes I want to use some packages installed with npm in the browser, but loaded as native ES6 modules, so I don't need webpack, babel or any other transpiler/packer/whatever, simply copy these files and that's all.
+
+## How it works?
+
+You only need to define the packages you want to install and the destination folder. This tool will search the `package.json` file of each package and copy the files defined in [files field](https://docs.npmjs.com/files/package.json#files).
+
+If all files of a package are located in the same subdirectory, it strip that subdirectory. It's common to use subdirectories like `src`, `dist`, etc.
+
+## Usage
+
+```js
+const assets = require('browser-assets');
+
+assets(['package1', '@vendor/package2', 'package3'], 'public/vendor')
+	.then(() => console.log('Vendor files copied!'))
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+const resolvePkg = require('resolve-pkg');
+const path = require('path');
+const globby = require('globby');
+const fs = require('fs-extra');
+
+async function package(name, dest) {
+    const dir = resolvePkg(name);
+
+    if (!dir) {
+        throw new Error(`The package ${name} was not found`);
+    }
+
+    const fromFiles = await getPackageFiles(dir);
+    const toFiles = stripBasePath(fromFiles);
+
+    return Promise.all(
+        fromFiles.map((fromFile, i) => {
+            const from = path.join(dir, fromFile);
+            const to = path.join(dest, name, toFiles[i]);
+
+            return fs.copy(from, to);
+        })
+    );
+}
+
+async function copy(packages, dest = 'vendor') {
+    await fs.remove(dest);
+
+    return Promise.all(packages.map(name => package(name, dest)));
+}
+
+function getPackageFiles(dir) {
+    const packageFile = path.join(dir, 'package.json');
+    const package = require(packageFile);
+
+    if (package.files) {
+        return globby(package.files, { cwd: dir });
+    }
+
+    throw new Error(`No "files" field found in ${packageFile}`);
+}
+
+function stripBasePath(files) {
+    while (files[0].includes(path.sep)) {
+        const base = files[0].split(path.sep, 2).shift() + path.sep;
+
+        if (files.every(file => file.startsWith(base))) {
+            files = files.map(file => file.substr(base.length));
+        } else {
+            break;
+        }
+    }
+
+    return files;
+}
+
+module.exports = copy;
